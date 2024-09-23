bdeb50119f0bd72dc0f09df96e8bfc61c4a8eb65
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-09-23 19:38:54 -0400

    feat: navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assests/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const Menu = () => (
+    <>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#wgpt">What is GPT?</a></li>
+        <li><a href="#possibility">Open AI</a></li>
+        <li><a href="#features">Case Studies</a></li>
+        <li><a href="#blog">Library</a></li>
+      </>
+  );
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
-          <ul className="gpt3__navbar-links_container ">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#wgpt">What is GPT?</a></li>
-            <li><a href="#possibility">Open AI</a></li>
-            <li><a href="#features">Case Studies</a></li>
-            <li><a href="#blog">Library</a></li>
-        </ul>
-        <div className="gpt3__navbar-sign">
-          <button className="gpt3__navbar-sign">Sign in</button>
-          <button className="gpt3__navbar-sign_up">Sign up</button>
-        </div>
+        <ul className="gpt3__navbar-links_container ">
+           <Menu />
+          </ul>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <a aria-label="sign in" role="button" href="#">Sign in</a>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27}  onClick={() => setToggleMenu(false)}/>
+          : <RiMenu3Line color="#fff" size={27}  onClick={() => setToggleMenu(true)}/>
+       }
+        { toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container_links">
+              <ul className="gpt3__navbar-menu_container_links-list">
+                <Menu />
+              </ul>
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <a aria-label="sign in" role="button" href="#">Sign in</a>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+            </div>
+        )}
       </div>
     </div>
   )
