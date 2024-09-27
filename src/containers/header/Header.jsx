1491d8f684770c16cf09f4dba578a05f3659bed8
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-09-27 07:44:48 -0400

    fix: move div to fix img position

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -18,10 +18,10 @@ const Header = () => {
           <img src={people} alt='people' />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className='gpt3__header-content__ai'>
-          <img src={ai} alt='ai' />
-          <p>AI models created with OpenAI GPT-3</p>  
-         </div>
+      </div>
+      <div className='gpt3__header-content__ai'>
+        <img src={ai} alt='ai' />
+        <p>AI models created with OpenAI GPT-3</p>  
       </div>
     </section>
   )
