4be8bb673b16ce4b7f33a4a19ad3254526fe331b
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-09-26 23:53:36 -0400

    feat: header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import people from '../../assests/people.png';
+import ai from '../../assests/ai.png';
 import './header.css';
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='home' className='gpt3__header section__padding'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your email address' />
+          <button type='button'>Get Started</button>
+        </div>  
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className='gpt3__header-content__ai'>
+          <img src={ai} alt='ai' />
+          <p>AI models created with OpenAI GPT-3</p>  
+         </div>
+      </div>
+    </section>
   )
 }
 
