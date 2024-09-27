f33fd8b9ba7c6a5db387b9d93699a688ec8baa3c
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-09-27 08:51:42 -0400

    feat: gather up all the  images for easy import (helper file)

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import atlassian from '../../assests/atlassian.png';
+import dropbox from '../../assests/dropbox.png';
+import google from '../../assests/google.png';
+import shopify from '../../assests/shopify.png';
+import slack from '../../assests/slack.png';
+
+export {
+  atlassian,
+  dropbox,
+  google,
+  shopify,
+  slack
+}
