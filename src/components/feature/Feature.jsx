1480867248239cd4cd017f032d70a0cb14f20b6f
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-09-27 22:11:37 -0400

    feat: bare/skeleton for fetures also stopping point

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './feature.css';
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>
-      
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__featutes-container-">
+        <div />
+        <h1>{ title }</h1>
+      </div>
+      <div className="gpt3__features-container">
+        {text}
+      </div>
     </div>
   )
 }
 
-export default Feature
+export default Feature;
